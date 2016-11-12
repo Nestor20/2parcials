291eef9a3d27d02d612ca4676c2d7b2fca88f005
Nestor20 <nikajunior19gmail.com>
2016-11-12 10:21:07 -0400

    iniciar repo

---
+++ Igualdad.js
@@ -0,0 +1,3 @@
+console.log(1<2);
+console.log(!6==6);
+console.log("6" ==="7");
---
+++ objetoejemplo.js
@@ -0,0 +1,2 @@
+var datos={nombre:"mario" , apellido:"loaza", telefono:78770009, direccion:"san jose"}
+console.log("nombre:"+datos.nombre,"apellido:"+datos.apellido,"telefono:"+datos.telefono);
